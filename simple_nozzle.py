2d61b1b8e848a7974af9127eb40fcb1bb154a9d9
Gray <jsgray@GRSLA40018180.ndc.nasa.gov>
2013-07-30 10:11:01 -0400

    apparently, I was drunk for my last commit

---
+++ simple_nozzle.py
@@ -0,0 +1,54 @@
+import time
+import numpy as np
+
+from stl import STL
+from ffd import Body, Shell
+from geometry import Geometry
+
+
+start_time = time.time()
+
+plug = STL('nozzle/plug.stl')
+cowl = STL('nozzle/cowl.stl')
+
+print "STL Load Time: ", time.time()-start_time
+start_time = time.time()
+
+
+
+n_c = 10
+body = Body(plug,controls=n_c) #just makes n_C evenly spaced points
+shell = Shell(cowl,cowl.copy(),n_c,n_c)
+
+geom = Geometry()
+geom.add(body,name="plug")
+geom.add(shell,name="cowl")
+
+print "Bspline Compute Time: ", time.time()-start_time
+start_time = time.time()
+
+deltaC_x = np.zeros((n_c,))
+deltaC_r = np.zeros((n_c,))
+deltaC_x[3:] = -3 #second to last element, set to 10
+deltaC = np.array(zip(deltaC_x,deltaC_r))
+geom.deform(plug=deltaC)
+
+
+deltaC_cx = np.zeros((n_c,))
+deltaC_cr = np.zeros((n_c,))
+deltaC_cr[:] = 1 #second to last element, set to 10
+deltaC_c = np.array(zip(deltaC_cx,deltaC_cr))
+deltaC_tx = np.zeros((n_c,))
+deltaC_tr = np.zeros((n_c,))
+deltaC_tr[:-1] = .25 #second to last element, set to 10
+deltaC_t = np.array(zip(deltaC_tx,deltaC_tr))
+geom.deform(cowl=(deltaC_c,deltaC_t))
+
+
+print "Run Time: ", time.time()-start_time
+start_time = time.time()
+    
+geom.writeSTL('new.stl', ascii=False)
+
+print "STL Write Time: ", time.time()-start_time
+start_time = time.time()
